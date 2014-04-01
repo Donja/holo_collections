e7d9ef77ada48a1b55f2f3ed739e6ef949e14425
Jay <jmendoza67@academic.rrc.ca>
2014-03-31 23:21:54 -0500

    Added manufacturer association to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params     
-      params.require(:product).permit(:name, :manufacturer, :category_id, :description, :price, :stock_quantity, :image_filname)
+      params.require(:product).permit(:name, :manufacturer_id, :category_id, :description, :price, :stock_quantity, :image_filname)
     end
 end
