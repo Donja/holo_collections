d345214eceda67d599d62f1e2d82d6b8a2154b50
Jay <jmendoza67@academic.rrc.ca>
2014-03-31 19:50:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoloCollections::Application.config.secret_key_base = '1858dee6edeee11c921b1d67063148e55c36e0ab506c35c6d6783e96598a569461ca263f729a973c58bbd69ac637379d74e1d9930155c0168b1b168fec91cf51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HoloCollections::Application.config.session_store :cookie_store, key: '_holo_collections_session'
