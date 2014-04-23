9282fdca92eb56ea7c9cd377c839a0acbe48d354
Donja <glorp@live.com>
2014-04-23 11:15:07 -0500

    Added remove images option to products in active admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,24 +12,29 @@ class Product < ActiveRecord::Base
   
   belongs_to :category
   belongs_to :manufacturer
-  before_save :destroy_image?
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   #do_not_validate_attachment_file_type :image
   
+  #before_save :destroy_image?
+  
+  before_validation { self.image.destroy if self.image_delete = '1' }
+  
+  def image_delete=(value)
+    if value == '1'
+      self.image = nil
+    end
+  end
+  
   def image_delete
-    @image_delete ||= "0"
   end
-  
-  def image_delete (val)
-    @image_delete = val
+    
+  def self.search(search)
+    search_condition = "%" + search + "%"
+    find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
   end
-  
-  private
-  def destroy_image?
-    self.image.clear if @image_delete == "1"
-  end
+    
 end
 
 
